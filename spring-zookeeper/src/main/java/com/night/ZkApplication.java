b6e4bb16c816273eea6c17f838da3af792321500
CharmNight <499715548@qq.com>
2022-02-22 18:51:14 +0800

    init project

---
+++ spring-zookeeper/src/main/java/com/night/ZkApplication.java
@@ -0,0 +1,14 @@
+package com.night;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author night
+ */
+@SpringBootApplication
+public class ZkApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZkApplication.class, args);
+    }
+}
